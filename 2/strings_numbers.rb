331fdfca710eedd597b7cebdab4fa6b1c7025581
romigos <bolistovskiyroman@gmail.com>
2020-06-12 16:46:10 +0300

    2-3 lessons

---
+++ 2/strings_numbers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+puts 'string'.class
+puts '1'.class
+puts 2.class
+
+puts '1' + '2'
+puts 1 + 2
---
+++ 2/valiables.rb
@@ -0,0 +1,3 @@
+name = 'Roman'
+position = 'Dev'
+puts 'My name is ' + name + '. My position is ' + position
